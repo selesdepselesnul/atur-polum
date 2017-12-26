9fba852251051c060d0488184d39b34ed0dc1976
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-26 21:37:37 +0700

    add command line arg

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import Sound.ALSA.Mixer
 import qualified Control.Monad as CM
+import qualified System.Environment as Environment
 
 changeVolumeBy :: Integer -> IO ()
 changeVolumeBy i =
@@ -8,14 +9,22 @@ changeVolumeBy i =
       do Just control <- getControlByName mixer "Master"
          let Just playbackVolume = playback $ volume control
          (min, max) <- getRange playbackVolume
-         Just volLeft <- getChannel FrontLeft $ value $ playbackVolume
-         Just volRight <- getChannel FrontRight $ value $ playbackVolume
-         CM.when True
-                 (setChannel FrontLeft (value $ playbackVolume) $ volLeft + i) >>
-                 (setChannel FrontRight (value $ playbackVolume) $ volRight + i)
+         vol <- getChannel FrontLeft $ value $ playbackVolume
+         case vol of
+           Just x -> 
+             CM.when True
+                 (setChannel FrontLeft (value $ playbackVolume) (x + i) )
+           _ -> putStrLn "failed"
 
 main :: IO ()
 main = do
-  putStrLn "atur-polum"
-
+    args <- Environment.getArgs
+    if length args == 0 then
+      putStrLn "please, fill the argument"
+    else
+      handlePolum $ head args
+    where handlePolum arg = 
+           case arg of
+           "+" -> changeVolumeBy 1
+           _ -> putStrLn "argument doesnt valid" 
 
